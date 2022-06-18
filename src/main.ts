d8cff13930410e444112ed867e5afaa5018ea07c
Hugo Averty <hugoaverty@gmail.com>
2022-06-18 17:36:42 +0200

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,182 @@
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
+import {getLayersMap} from '@workadventure/scripting-api-extra';
+
+const layers = await getLayersMap();
+
+const homeOKTopLeft3 = layers.get('homeOKTopLeft3');
+const homeOKTopLeft2 = layers.get('homeOKTopLeft2');
+const homeOKTopLeft1 = layers.get('homeOKTopLeft1');
+
+const homeOKBotLeft3 = layers.get('homeOKBotLeft3');
+const homeOKBotLeft2 = layers.get('homeOKBotLeft2');
+const homeOKBotLeft1 = layers.get('homeOKBotLeft1');
+
+const homeOKTopRight3 = layers.get('homeOKTopRight3');
+const homeOKTopRight2 = layers.get('homeOKTopRight2');
+const homeOKTopRight1 = layers.get('homeOKTopRight1');
+
+const homeOKBotRight3 = layers.get('homeOKBotRight3');
+const homeOKBotRight2 = layers.get('homeOKBotRight2');
+const homeOKBotRight1 = layers.get('homeOKBotRight1');
+
+
+
+
+
 console.log('Script started successfully');
 
 let currentPopup: any = undefined;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
 
-    WA.room.onEnterLayer('clockZone').subscribe(() => {
+    /* BUGGY
+    console.log("START");
+    const layers = await getLayersMap();
+
+    // Access a layer directly by name
+    const mylayer = layers.get('TEST');
+
+    // Iterate over all layers
+    for (const layer of layers.values()) {
+        console.log(layer);
+    }
+    console.log("END");
+    */
+
+    // TOP LEFT ZONE
+    WA.room.onEnterLayer('topleftZone').subscribe(() => {
         const today = new Date();
         const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
+        currentPopup = WA.ui.openPopup("topleftPopup","L’association Toit à Moi œuvre en faveur :",[{
+            label: "Des réfugiés politiques",
+            className: "normal",
+            callback: (popup) => {
+
+            }
+        },
+        {
+            label: "Des personnes sans-abri",
+            className: "normal",
+            callback: (popup) => {
+                // Close the popup when the "Close" button is pressed
+                WA.room.showLayer("homeOKTopLeft1");
+                WA.room.showLayer("homeOKTopLeft2");
+                WA.room.showLayer("homeOKTopLeft3");
+                popup.close();
+            }
+        },
+        {
+            label: "Des mineurs isolés",
+            className: "normal",
+            callback: (popup) => {
+
+            }
+        },
+        {
+            label: "Des femmes",
+            className: "normal",
+            callback: (popup) => {
+
+            }
+        },
+        ]);
     })
 
-    WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
+    WA.room.onLeaveLayer('topleftZone').subscribe(closePopUp)
+
+    // TOP RIGHT ZONE
+    WA.room.onEnterLayer('toprightZone').subscribe(() => {
+        const today = new Date();
+        const time = today.getHours() + ":" + today.getMinutes();
+        currentPopup = WA.ui.openPopup("toprightPopup"," L’association Habitâge intervient en priorité auprès de personnes âgées",[{
+            label: "En milieu rural",
+            className: "normal",
+            callback: (popup) => {
+                // Close the popup when the "Close" button is pressed
+                WA.room.showLayer("homeOKTopRight1");
+                WA.room.showLayer("homeOKTopRight2");
+                WA.room.showLayer("homeOKTopRight3");
+                popup.close();
+            }
+        },
+        {
+            label: "En milieu urbain",
+            className: "normal",
+            callback: (popup) => {
+
+            }
+        },
+        {
+            label: "Vivant dans des ZAC (Zones d’Aménagement Concerté)",
+            className: "normal",
+            callback: (popup) => {
+
+            }
+        },
+        {
+            label: "En Ile-de-France",
+            className: "normal",
+            callback: (popup) => {
+
+            }
+        },
+        ]); 
+    })
+
+    WA.room.onLeaveLayer('toprightZone').subscribe(closePopUp)
+
+    // BOT LEFT ZONE
+    WA.room.onEnterLayer('botleftZone').subscribe(() => {
+        const today = new Date();
+        const time = today.getHours() + ":" + today.getMinutes();
+        currentPopup = WA.ui.openPopup("botleftPopup","Quel lieu historique a été investi par l’association Le Village de François ?",
+        [{
+            label: "Un stade de foot",
+            className: "normal",
+            callback: (popup) => {
+
+            }
+            },
+            {
+                label: "Une école",
+                className: "normal",
+                callback: (popup) => {
+
+                }
+            },
+            {
+                label: "Une ferme",
+                className: "normal",
+                callback: (popup) => {
+
+                }
+            },
+            {
+                label: "Une abbaye",
+                className: "normal",
+                callback: (popup) => {
+                    // Close the popup when the "Close" button is pressed
+                    WA.room.showLayer("homeOKBotLeft1");
+                    WA.room.showLayer("homeOKBotLeft2");
+                    WA.room.showLayer("homeOKBotLeft3");
+                    popup.close();
+                }
+            },
+        ]); 
+    })
+
+    WA.room.onLeaveLayer('botleftZone').subscribe(closePopUp)
+
+    WA.room.onEnterLayer('botrightZone').subscribe(() => {
+        const today = new Date();
+        const time = today.getHours() + ":" + today.getMinutes();
+        currentPopup = WA.ui.openPopup("botrightPopup","BOT RIGHT",[]);
+    })
+
+    WA.room.onLeaveLayer('botrightZone').subscribe(closePopUp)
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
